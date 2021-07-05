67584fe8c2b1f51a69b2d03fc6b77f067f684c32
Belle Duran <bduran04@gmail.com>
2021-07-05 14:02:22 -0500

    updated the avatar in the about me page

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Container from '@material-ui/core/Container';
 import './style.css';
+import pic from '../../images/avatar.png'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -10,8 +11,8 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: "center",
         '& > *': {
             margin: theme.spacing(1),
-            width: theme.spacing(9),
-            height: theme.spacing(9),
+            width: theme.spacing(20),
+            height: theme.spacing(25),
         },
     },
 }));
@@ -21,12 +22,11 @@ function AboutMe(props) {
     return (
         <Container className="introduction" style={{ height: '40vh' }}>
             <div className={classes.root}>
-                <Avatar src="" />
+                <Avatar alt="Belle" src={pic} />
             </div>
             <p className="introduction-text">
                 Loves cats, gardening, and authentic Mexican cuisine.
-                My grandma's tamales can throw down with Bobby Flay;
-                currently residing in Plano, Texas.
+                My grandma's tamales can throw down with Bobby Flay and Gordon Ramsay.
             </p>
         </Container>
     )
