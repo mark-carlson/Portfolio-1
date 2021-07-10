ebe693e2800d81526937c252045430a30fd56239
Belle Duran <bduran04@gmail.com>
2021-07-09 22:26:06 -0500

    added links to the project cards

--- src/Projects/ProjectCards.js
+++ src/Projects/ProjectCards.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import Link from '@material-ui/core/Link';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -13,7 +13,7 @@ import Grid from '@material-ui/core/Grid';
 const useStyles = makeStyles((theme) => ({
   media: {
     height: 0,
-    paddingTop: '50%', 
+    paddingTop: '50%',
   },
   expand: {
     transform: 'rotate(0deg)',
@@ -30,42 +30,33 @@ const useStyles = makeStyles((theme) => ({
 export default function ProjectCard(props) {
   console.log(props)
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   return (
     <Grid item xs={6}>
-    <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        style={{
-          backgroundImage: props.image ? `url(${props.image})` : "none"
-        }}
-        title="Project"
-      />
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {props.description}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <GitHubIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-        </IconButton>
-      </CardActions>
-    </Card>  
+      <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          style={{
+            backgroundImage: props.image ? `url(${props.image})` : "none"
+          }}
+          title="Project"
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.description}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton >
+            <Link href={props.link} color="inherit">
+              <GitHubIcon />
+            </Link>
+          </IconButton>
+          <IconButton>
+
+          </IconButton>
+        </CardActions>
+      </Card>
     </Grid>
   );
 }
